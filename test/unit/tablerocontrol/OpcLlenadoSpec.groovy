2c75ecac45e5f55ba99557929507249c93d13fb9
Alejandro Ortiz <holaalex2204@hotmail.com>
2014-08-26 15:51:28 -0500

    Agregar dominios Etapa, Fase, Opcion Llenado

---
+++ test/unit/tablerocontrol/OpcLlenadoSpec.groovy
@@ -0,0 +1,20 @@
+package tablerocontrol
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(OpcLlenado)
+class OpcLlenadoSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
